242915fc8bbfc7e7743d9ff130924e7bf50517b3
Beck-Sisyphus <pshfls@gmail.com>
2017-08-24 22:18:45 +0800

    SWSTM32 works, test If MDK-ARM V5 works again

--- STM32F427IIHx.cmake
+++ STM32F427IIHx.cmake
@@ -16,7 +16,7 @@ SET(LINKER_SCRIPT ${CMAKE_SOURCE_DIR}/STM32F427IIHx_FLASH.ld)
 # Options for gcc
 # https://gcc.gnu.org/onlinedocs/gcc/Option-Summary.html
 #SET(COMMON_FLAGS "-mcpu=cortex-m4 -mthumb -mthumb-interwork -mfloat-abi=soft -ffunction-sections -fdata-sections -g -fno-common -fmessage-length=0") -specs=nano.specs
-SET(COMMON_FLAGS "-mcpu=cortex-m4 -mthumb -mthumb-interwork -mfloat-abi=hard -mfpu=fpv4-sp-d16 -ffunction-sections -fdatda-sections -g -fno-common -fmessage-length=0 -specs=nosys.specs")
+SET(COMMON_FLAGS "-mcpu=cortex-m4 -mthumb -mthumb-interwork -mfloat-abi=hard -mfpu=fpv4-sp-d16 -ffunction-sections -g -fno-common -fmessage-length=0 -specs=nosys.specs")
 SET(CMAKE_CXX_FLAGS_INIT "${COMMON_FLAGS} -std=c++11")
 SET(CMAKE_C_FLAGS_INIT "${COMMON_FLAGS} -std=gnu99")
 SET(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,-gc-sections,-Map=binary.map -T ${LINKER_SCRIPT}")
